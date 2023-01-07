6c75281934f0439baf3f804ab74ae4f721e6ef18
Dmitry Momot <mail@dmomot.com>
2023-01-08 01:31:50 +0200

    refactored functions parameters

--- transaction.go
+++ transaction.go
@@ -4,8 +4,96 @@ import (
 	"context"
 
 	"github.com/pkg/errors"
+	"github.com/portto/solana-go-sdk/common"
+	"github.com/portto/solana-go-sdk/program/system"
 	"github.com/portto/solana-go-sdk/types"
 )
+
+// NewTransactionParams is the params for NewTransaction function.
+type NewTransactionParams struct {
+	Base58FeePayerAddr string
+	Instructions       []types.Instruction
+}
+
+// NewTransaction creates a new transaction.
+// Returns the transaction or an error.
+func (c *Client) NewTransaction(ctx context.Context, params NewTransactionParams) ([]byte, error) {
+	latestBlockhash, err := c.solana.GetLatestBlockhash(ctx)
+	if err != nil {
+		return nil, errors.Wrap(ErrGetLatestBlockhash, err.Error())
+	}
+
+	tx, err := types.NewTransaction(types.NewTransactionParam{
+		Message: types.NewMessage(types.NewMessageParam{
+			FeePayer:        common.PublicKeyFromString(params.Base58FeePayerAddr),
+			RecentBlockhash: latestBlockhash.Blockhash,
+			Instructions:    params.Instructions,
+		}),
+	})
+	if err != nil {
+		return nil, errors.Wrap(ErrNewTransaction, err.Error())
+	}
+
+	txb, err := tx.Serialize()
+	if err != nil {
+		return nil, errors.Wrap(ErrSerializeTransaction, err.Error())
+	}
+
+	return txb, nil
+}
+
+// NewDurableTransactionParams are the parameters for NewDurableTransaction function.
+type NewDurableTransactionParams struct {
+	Base58FeePayerAddr     string
+	Base58NonceAuthAddr    string
+	Base58DurableNonceAddr string
+	Instructions           []types.Instruction
+}
+
+// NewDurableTransaction creates a new durable transaction.
+// base58FeePayerAddr is the base58 encoded fee payer address.
+// base58DurableNonceAddr is the base58 encoded durable nonce address.
+// instructions is the transaction instructions.
+// Returns the serialized transaction or an error.
+func (c *Client) NewDurableTransaction(ctx context.Context, params NewDurableTransactionParams) ([]byte, error) {
+	nonce, err := c.solana.GetNonceFromNonceAccount(ctx, params.Base58DurableNonceAddr)
+	if err != nil {
+		return nil, errors.Wrap(ErrGetNonceFromNonceAccount, err.Error())
+	}
+
+	feePayerPublicKey := common.PublicKeyFromString(params.Base58FeePayerAddr)
+
+	base58NonceAuthAddr := params.Base58NonceAuthAddr
+	if base58NonceAuthAddr == "" {
+		base58NonceAuthAddr = params.Base58FeePayerAddr
+	}
+
+	instr := []types.Instruction{
+		system.AdvanceNonceAccount(system.AdvanceNonceAccountParam{
+			Nonce: common.PublicKeyFromString(params.Base58DurableNonceAddr),
+			Auth:  common.PublicKeyFromString(base58NonceAuthAddr),
+		}),
+	}
+	instr = append(instr, params.Instructions...)
+
+	tx, err := types.NewTransaction(types.NewTransactionParam{
+		Message: types.NewMessage(types.NewMessageParam{
+			FeePayer:        feePayerPublicKey,
+			RecentBlockhash: nonce,
+			Instructions:    instr,
+		}),
+	})
+	if err != nil {
+		return nil, errors.Wrap(ErrNewTransaction, err.Error())
+	}
+
+	txb, err := tx.Serialize()
+	if err != nil {
+		return nil, errors.Wrap(ErrSerializeTransaction, err.Error())
+	}
+
+	return txb, nil
+}
 
 // GetTransactionFee gets the fee for a transaction.
 // Returns the fee or error.
@@ -62,4 +150,29 @@ func (c *Client) SendTransaction(ctx context.Context, txSource []byte) (string, 
 	}
 
 	return txhash, nil
+}
+
+// GetTransactionStatus gets the transaction status.
+// Returns the transaction status or an error.
+func (c *Client) GetTransactionStatus(ctx context.Context, txhash string) (TransactionStatus, error) {
+	status, err := c.solana.GetSignatureStatus(ctx, txhash)
+	if err != nil {
+		return TransactionStatusUnknown, errors.Wrap(ErrGetTransactionStatus, err.Error())
+	}
+
+	if status.Err != nil {
+		return TransactionStatusFailure, nil
+	}
+
+	result := TransactionStatusUnknown
+
+	if status.Confirmations != nil && *status.Confirmations > 0 {
+		result = TransactionStatusInProgress
+	}
+
+	if status.ConfirmationStatus != nil {
+		result = ParseTransactionStatus(*status.ConfirmationStatus)
+	}
+
+	return result, nil
 }
