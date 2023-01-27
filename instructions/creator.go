a72ede9335385c939f8f2381785d00fd61a73b41
Dmitry Momot <mail@dmomot.com>
2023-01-27 10:36:27 +0200

    + remove creator verification instruction

--- instructions/creator.go
+++ instructions/creator.go
@@ -50,3 +50,43 @@ func VerifyCreator(params VerifyCreatorParams) InstructionFunc {
 		return instructions, nil
 	}
 }
+
+// RemoveCreatorVerificationParams is the params for RemoveCreatorVerification
+type RemoveCreatorVerificationParams struct {
+	Mint    common.PublicKey // required; The mint of the token
+	Creator common.PublicKey // required; The creator of the token
+}
+
+// Validate validates the params.
+func (p RemoveCreatorVerificationParams) Validate() error {
+	if p.Mint == (common.PublicKey{}) {
+		return fmt.Errorf("mint is required")
+	}
+	if p.Creator == (common.PublicKey{}) {
+		return fmt.Errorf("creator is required")
+	}
+	return nil
+}
+
+// RemoveCreatorVerification removes the creator verification of the token metadata.
+func RemoveCreatorVerification(params RemoveCreatorVerificationParams) InstructionFunc {
+	return func() ([]types.Instruction, error) {
+		if err := params.Validate(); err != nil {
+			return nil, fmt.Errorf("remove creator verification: %w", err)
+		}
+
+		tokenMetadataPubkey, err := token_metadata.DeriveTokenMetadataPubkey(params.Mint)
+		if err != nil {
+			return nil, fmt.Errorf("failed to derive token metadata pubkey: %w", err)
+		}
+
+		return []types.Instruction{
+			metaplex_token_metadata.RemoveCreatorVerification(
+				metaplex_token_metadata.RemoveCreatorVerificationParam{
+					Metadata: tokenMetadataPubkey,
+					Creator:  params.Creator,
+				},
+			),
+		}, nil
+	}
+}
